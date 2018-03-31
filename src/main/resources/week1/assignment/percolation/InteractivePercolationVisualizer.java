9ef85e04f8371b77b6d6c23e704da47d2beb3162
dincau33 <dincau33@gmail.com>
2018-03-31 11:43:34 -0400

    Implement percolation problem solution

--- src/main/resources/week1/assignment/percolation/InteractivePercolationVisualizer.java
+++ src/main/resources/week1/assignment/percolation/InteractivePercolationVisualizer.java
@@ -11,8 +11,8 @@
  *
  ******************************************************************************/
 
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.StdOut;
 
 public class InteractivePercolationVisualizer {
 
--- src/main/resources/week1/assignment/percolation/PercolationVisualizer.java
+++ src/main/resources/week1/assignment/percolation/PercolationVisualizer.java
@@ -18,8 +18,8 @@
 
 import java.awt.Font;
 
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.StdDraw;
 
 public class PercolationVisualizer {
 
