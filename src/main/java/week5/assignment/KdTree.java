823806a3da859d2f5529f513e0097fb00ac6b4f9
dincau33 <dincau33@gmail.com>
2018-07-14 17:29:25 -0400

    Initialize week 5 assignment

---
+++ src/main/java/week5/assignment/KdTree.java
@@ -0,0 +1,54 @@
+package week5.assignment;
+
+import edu.princeton.cs.algorithms.Point2D;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class KdTree {
+
+	private int size = 0;
+	private TreeSet<Point2D> set = null;
+
+	// construct an empty set of points
+	public KdTree() {
+
+	}
+
+	// is the set empty?
+	public boolean isEmpty() {
+		return true;
+	}
+
+	// number of points in the set
+	public int size() {
+		return size;
+	}
+
+	// add the point to the set (if it is not already in the set)
+	public void insert(Point2D p) {
+
+	}
+
+	// does the set contain point p?
+	public boolean contains(Point2D p) {
+		return false;
+	}
+
+	// draw all points to standard draw
+	public void draw() {
+
+	}
+
+	// all points that are inside the rectangle (or on the boundary)
+	public Iterable<Point2D> range(RectHV rect) {
+		return new ArrayList<>();
+	}
+
+	// a nearest neighbor in the set to point p; null if the set is empty
+	public Point2D nearest(Point2D p) {
+		return new Point2D(0, 0);
+	}
+
+
+}
