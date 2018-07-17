7534068c24406561e51d0ace1aef761e98e75fed
dincau33 <dincau33@gmail.com>
2018-07-16 21:08:30 -0400

    Week 5 assignment: KdTree implements insert and contains

--- src/main/java/week5/assignment/KdTree.java
+++ src/main/java/week5/assignment/KdTree.java
@@ -5,11 +5,15 @@ import edu.princeton.cs.algorithms.Stack;
 
 public class KdTree {
 
+	private static final boolean VERTICAL_LINE = true;
+	private static final boolean HORIZONTAL_LINE = false;
+
 	private int size = 0;
 	private KdNode root = null;
 
 	// construct an empty set of points
-	public KdTree() {}
+	public KdTree() {
+	}
 
 	// is the set empty?
 	public boolean isEmpty() {
@@ -24,11 +28,63 @@ public class KdTree {
 	// add the point to the set (if it is not already in the set)
 	public void insert(Point2D p) {
 		if (p == null) throw new IllegalArgumentException();
+		root = insert(root, p, VERTICAL_LINE);
+	}
+
+	private KdNode insert(KdNode node, Point2D p, boolean line) {
+		if (node == null) {
+			size++;
+			return new KdNode(p);
+		}
+
+		if (node.p.equals(p)) return node;
+
+		if (line == VERTICAL_LINE) {
+			if (p.x() < node.p.x()) {
+				node.left = insert(node.left, p, HORIZONTAL_LINE);
+			} else {
+				node.right = insert(node.right, p, HORIZONTAL_LINE);
+			}
+		}
+
+		if (line == HORIZONTAL_LINE) {
+			if (p.y() < node.p.y()) {
+				node.left = insert(node.left, p, VERTICAL_LINE);
+			} else {
+				node.right = insert(node.right, p, VERTICAL_LINE);
+			}
+		}
+
+		return node;
 	}
 
 	// does the set contain point p?
 	public boolean contains(Point2D p) {
 		if (p == null) throw new IllegalArgumentException();
+		return contains(root, p, VERTICAL_LINE);
+	}
+
+	private boolean contains(KdNode node, Point2D p, boolean line) {
+		if (node == null) return false;
+
+		if (node.p.equals(p)) return true;
+
+		if (line == VERTICAL_LINE) {
+			if (p.x() < node.p.x()) {
+				return contains(node.left, p, HORIZONTAL_LINE);
+			} else {
+				return contains(node.right, p, HORIZONTAL_LINE);
+			}
+		}
+
+		if (line == HORIZONTAL_LINE) {
+			if (p.y() < node.p.y()) {
+				return contains(node.left, p, VERTICAL_LINE);
+			} else {
+				return contains(node.right, p, VERTICAL_LINE);
+			}
+		}
+
 		return false;
 	}
 
